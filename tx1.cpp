d9045a6bd88ebddb87340b8b2f460d0c95ebc2ab
alanpa2020 <61548019+alanpa2020@users.noreply.github.com>
2020-02-27 17:08:42 +0300

    removed comments from several lines

--- tx1.cpp
+++ tx1.cpp
@@ -8,14 +8,14 @@ int main()
     txSetFillColor (color_gray(90));
     txClear ();
     repka_up();
-    //dedus_move(800,180);
-//    txSleep (500);
-//    dedus_move(180,800);
-//    dedus_babus_move(800,180);
-//    babus_move(180+60,800);
-//    babus_vnuchkas_move(800,180+60);
-//    sharics_and_kiska_move(800,180+120);
-    //dedus_draw (120, 200, 2);
+    dedus_move(800,180);
+    txSleep (500);
+    dedus_move(180,800);
+    dedus_babus_move(800,180);
+    babus_move(180+60,800);
+    babus_vnuchkas_move(800,180+60);
+    sharics_and_kiska_move(800,180+120);
+
 //    //babus_draw (240, 200);
 //    vnuchkas_draw (360, 200);
 //    sharics_draw (480, 200);
